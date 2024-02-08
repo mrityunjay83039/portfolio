de42add6edeef487f0a369a58e96b64fc55b1fe4
mrityunjay <mrityunjay.83039@gmail.com>
2024-02-08 12:47:45 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily:{
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Inter', 'serif'],
+    },
+
+    extend: {
+      colors: {
+        'textColor': '#999999',
+        'bgDefault': '#1f1f1f',
+        'textWhite': '#ffffff',
+        'textPrimary': '#28e98c',
+      }
+    },
+  },
+  plugins: [],
+}
