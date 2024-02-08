7a9754c2246ceb8ad509b4743f5edf378b3166b8
mrityunjay <mrityunjay.83039@gmail.com>
2024-02-08 15:50:24 +0530

    tailwind configuration done with color, font, basic folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ProfileCard } from './components'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className=' text-red-500 font-serif text-regal-blue'>Hello Tailwind</h1>
-    </>
+    <main>
+      <aside>
+        <ProfileCard/>
+
+      </aside>
+      <section>
+        <p>hello</p>
+      </section>
+    </main>
   )
 }
 
---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProfileCard = () =>{
+    return(
+        <>sidebar</>
+    )
+}
+
+export default ProfileCard;
